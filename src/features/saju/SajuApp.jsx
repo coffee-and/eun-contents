b235909a0078f3d4cbe68f684cdfaac4d2ea8954
coffee-and <cheeseee.bagel@gmail.com>
2026-06-30 18:05:26 +0900

    style: load compact saju cards

--- src/features/saju/SajuApp.jsx
+++ src/features/saju/SajuApp.jsx
@@ -3,6 +3,7 @@ import { AppShell } from "../../shared/components/AppShell.jsx";
 import { FeatureHeader } from "../../shared/components/FeatureHeader.jsx";
 import { SajuContentSelector } from "./components/SajuContentSelector.jsx";
 import "./styles/saju.css";
+import "./styles/saju.compact.css";
 
 export default function SajuApp({ onNavigateHome }) {
   const [selectedContentId, setSelectedContentId] = useState("");
