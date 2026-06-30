97d5a113b7f490dd1bdea87dbb0371c258b912d1
coffee-and <cheeseee.bagel@gmail.com>
2026-06-30 17:50:51 +0900

    content: simplify mini games page header

--- src/features/mini-games/MiniGamesApp.jsx
+++ src/features/mini-games/MiniGamesApp.jsx
@@ -14,10 +14,8 @@ export default function MiniGamesApp({ onNavigateHome }) {
         copyClassName="mini-games-hero__copy"
         eyebrow="PLAY & MOMENT"
         eyebrowClassName="mini-games-hero__eyebrow"
-        title="가볍게 즐기는 게임"
+        title="미니 게임"
         titleClassName="mini-games-hero__title"
-        subtitle="잠깐의 여유가 필요할 때, 작은 게임으로 마음을 환기해요."
-        subtitleClassName="mini-games-hero__subtitle"
         onNavigateHome={onNavigateHome}
       />
 
